6af187a7f40260c87e0e41c1b529f779b3d2290e
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-12 04:27:57 +0000

    Them bang NhatKy - bỏ field time vì đã lưu giờ trong ngày.

---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/dto/NhatKy.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.cnpm02.dkhp.dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class NhatKy {
+    
+    private String maNK;
+    private String taiKhoan;
+    private String noiDung;
+    private Date ngayDang;
+
+    public NhatKy() {
+    }
+
+    public NhatKy(String maNK, String taiKhoan, String noiDung, Date ngayDang) {
+        this.maNK = maNK;
+        this.taiKhoan = taiKhoan;
+        this.noiDung = noiDung;
+        this.ngayDang = ngayDang;
+    }
+
+    public String getMaNK() {
+        return maNK;
+    }
+
+    public void setMaNK(String maNK) {
+        this.maNK = maNK;
+    }
+
+    public Date getNgayDang() {
+        return ngayDang;
+    }
+
+    public void setNgayDang(Date ngayDang) {
+        this.ngayDang = ngayDang;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
+
+    public String getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
+}
