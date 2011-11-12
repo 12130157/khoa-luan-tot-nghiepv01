b5bdab05558b3a2e7da221f6ed0592f74555813a
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-12 04:32:31 +0000

    Update bang Lop (lop.java)

--- KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/dto/Lop.java
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/dto/Lop.java
@@ -9,6 +9,76 @@ package uit.cnpm02.dkhp.dto;
  * @author thanh
  */
 public class Lop {
+    private String maLop;
+    private String tenLop;
+    private String maKhoa;
     
+    /**Mã khóa học**/
+    private String maKhoaHoc;
     
+    /**Ma giang vien chu nhiem**/
+    private String maGVCN;
+    
+    /**So luong sinh vien**/
+    private String SLSV;
+
+    public Lop() {
+    }
+
+    public Lop(String maLop, String tenLop, String maKhoa, String maKhoaHoc, String maGVCN, String SLSV) {
+        this.maLop = maLop;
+        this.tenLop = tenLop;
+        this.maKhoa = maKhoa;
+        this.maKhoaHoc = maKhoaHoc;
+        this.maGVCN = maGVCN;
+        this.SLSV = SLSV;
+    }
+
+    public String getSLSV() {
+        return SLSV;
+    }
+
+    public void setSLSV(String SLSV) {
+        this.SLSV = SLSV;
+    }
+
+    public String getMaGVCN() {
+        return maGVCN;
+    }
+
+    public void setMaGVCN(String maGVCN) {
+        this.maGVCN = maGVCN;
+    }
+
+    public String getMaKhoa() {
+        return maKhoa;
+    }
+
+    public void setMaKhoa(String maKhoa) {
+        this.maKhoa = maKhoa;
+    }
+
+    public String getMaKhoaHoc() {
+        return maKhoaHoc;
+    }
+
+    public void setMaKhoaHoc(String maKhoaHoc) {
+        this.maKhoaHoc = maKhoaHoc;
+    }
+
+    public String getMaLop() {
+        return maLop;
+    }
+
+    public void setMaLop(String maLop) {
+        this.maLop = maLop;
+    }
+
+    public String getTenLop() {
+        return tenLop;
+    }
+
+    public void setTenLop(String tenLop) {
+        this.tenLop = tenLop;
+    }
 }
