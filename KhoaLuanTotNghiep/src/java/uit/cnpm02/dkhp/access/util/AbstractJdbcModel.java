3cc0afceaaeaa85eccc6e0d93b766c3763ce1acb
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-12 15:45:09 +0000

    Update Mapper

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/access/util/AbstractJdbcModel.java
@@ -0,0 +1,68 @@
+package uit.cnpm02.dkhp.access.util;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * AbstractJdbcModel
+ * 
+ * @author See4sys
+ * 
+ * @param <ID> model's id type.
+ */
+public abstract class AbstractJdbcModel<ID extends Serializable> implements
+        IJdbcModel<ID> {
+
+    private static final long serialVersionUID = -4657416425482604941L;
+    private ID id;
+
+    @Override
+    public ID getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(ID id) {
+        this.id = id;
+    }
+    private Class<ID> idClass = null;
+
+    /**
+     * Default constructor.
+     */
+    @SuppressWarnings(value = {"unchecked"})
+    public AbstractJdbcModel() {
+        this.idClass = (Class<ID>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    @Override
+    public int getIdType() {
+        if (idClass.getSimpleName().equals("Integer")) {
+            return INT_ID_TYPE;
+        }
+        if (idClass.getSimpleName().equals("Long")) {
+            return LONG_ID_TYPE;
+        }
+        return STRING_ID_TYPE;
+    }
+
+    @Override
+    public boolean isIdAutoIncrement() {
+        return true;
+    }
+
+    @Override
+    public String getTableName() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getIdColumnName() {
+        return "ID";
+    }
+
+    @Override
+    public boolean validate() {
+        return true;
+    }
+}
---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/KhoaHoc.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.cnpm02.dkhp.model;
+
+/**
+ *
+ * @author thanh
+ */
+public class KhoaHoc {
+    String maKhoaHoc;
+    int namVao;
+    int namRa;
+    float hocKy;
+    int soSinhVien;
+    public KhoaHoc(){
+        
+    }
+    public KhoaHoc(String _maKhoaHoc, int _namVao, int _namRa, float _hocKy, int _soSinhVien){
+        this.maKhoaHoc=_maKhoaHoc;
+        this.namVao=_namVao;
+        this.namRa=_namRa;
+        this.hocKy=_hocKy;
+        this.soSinhVien=_soSinhVien;
+    }
+    //************
+    //set parameter
+    //************
+    public void setMaKhoa(String _maKhoa){
+        this.maKhoaHoc=_maKhoa;
+    }
+    public void setNamVao(int _namVao){
+        this.namVao=_namVao;
+    }
+    public void setNamRa(int _namRa){
+        this.namRa=_namRa;
+    }
+    public void setHocKy(float _hocKy){
+        this.hocKy=_hocKy;
+    }
+    public void setSoSinhVien(int _soSinhVien){
+        this.soSinhVien=_soSinhVien;
+    }
+    //************
+    //get parameter
+    //************
+    public String getMaKhoa(){
+        return this.maKhoaHoc;
+    }
+    public int getNamVao(){
+        return this.namVao;
+    }
+    public int getNamRa(){
+        return this.namRa;
+    }
+    public float getHocKy(){
+       return this.hocKy;
+    }
+    public int getSoSinhVien(){
+        return this.soSinhVien;
+    }
+    
+}
