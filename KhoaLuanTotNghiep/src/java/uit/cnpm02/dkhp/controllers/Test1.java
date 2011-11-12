9a710fd49a0a79b7273c1eaa140cec9cdb87d604
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-12 17:04:07 +0000

    Update test controller.

--- KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/Test1.java
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/Test1.java
@@ -2,6 +2,7 @@ package uit.cnpm02.dkhp.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,7 +11,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import uit.cnpm02.dkhp.DAO.AccountDAO;
 import uit.cnpm02.dkhp.DAO.NewsDAO;
+import uit.cnpm02.dkhp.model.Account;
 import uit.cnpm02.dkhp.model.News;
 /**
  *
@@ -18,6 +21,8 @@ import uit.cnpm02.dkhp.model.News;
  */
 @WebServlet(name = "Test1", urlPatterns = {"/Test1"})
 public class Test1 extends HttpServlet {
+    private AccountDAO accountDao = new AccountDAO();
+    private NewsDAO newsDao = new NewsDAO();
 
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -30,10 +35,10 @@ public class Test1 extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+        
+        testAddNews(out);
+        
         try {
-            //BanTinBO banTinBO = new BanTinBO();
-            //List<BanTin> news = banTinBO.getAll();
-            NewsDAO newsDao = new NewsDAO();
             List<News> news = newsDao.findAll();
             
             out.println("<html>");
@@ -103,4 +108,44 @@ public class Test1 extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private void testAddNews(PrintWriter out) {
+        News news = new News("BT06", "Nghi hoc chieu thu 7", "Thong bao cho sinh vien CNPM 02, tat ca dc nghi ngay chu nhat.", "Loc Nguyen", new Date(), 0);
+        int id = 0;
+        try {
+            id = newsDao.add(news);
+        } catch (Exception ex) {
+            Logger.getLogger(Test1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            News result = newsDao.findById(id);
+            
+            if(result != null) {
+                out.println(result.getTitle() + " --- " + result.getContent());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Test1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    private void testAdd(PrintWriter out) {
+        Account acc = new Account("locnv", "123", "Nguyen Van Loc", 0, "Normal", 1);
+        int id = -1;
+        try {
+            id = accountDao.add(acc);
+        } catch (Exception ex) {
+            Logger.getLogger(Test1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            Account result = accountDao.findById(id);
+            
+            if(result != null) {
+                out.println(result.getUserName() + " --- " + result.getPassword());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Test1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
