94887bc5c637b391d41a0b992cc8dd03a9f5809b
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-13 04:28:42 +0000

    Add AccountBO

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/bo/AccountBO.java
@@ -0,0 +1,72 @@
+package uit.cnpm02.dkhp.bo;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import uit.cnpm02.dkhp.DAO.AccountDAO;
+import uit.cnpm02.dkhp.model.Account;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class AccountBO {
+
+    private AccountDAO accDao = new AccountDAO();
+
+    public boolean Login(String userName, String password) {
+        try {
+
+            Account acc = accDao.findById(userName);
+
+            if ((acc != null)
+                    && (acc.getUserName().equals(userName))
+                    && (acc.getPassword().equals(password))) {
+                return true;
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AccountBO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+    }
+
+    public boolean isLogined(String user) {
+        try {
+            Account acc = accDao.findById(user);
+            if(acc != null) {
+                return acc.getIsLogined();
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AccountBO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public boolean isLock(String user) {
+        try {
+            Account acc = accDao.findById(user);
+            if(acc != null) {
+                return acc.getIsLogined();
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AccountBO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public boolean Logout(String userName, String password) {
+        return false;
+    }
+
+    /**
+     *
+     * @param user
+     * @param newpass
+     * @throws Exception
+     */
+    public void changePassWord(String user, String newpass) throws Exception {
+        Account acc = accDao.findById(user);
+        acc.setPassword(newpass);
+        accDao.update(acc);
+    }
+}
