5a03dc5f2ebca4f1b55631fa30f0eed3d8b39ee1
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-13 04:38:42 +0000

    Update Account Model

--- KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/Account.java
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/Account.java
@@ -11,14 +11,14 @@ public class Account extends AbstractJdbcModel<String> {
     private String userName;
     private String password;
     private String fullName;
-    private int isLogined;//1 dang dang nhap, 0 chua dang nhap
+    private boolean isLogined;//true dang dang nhap, false chua dang nhap
     private String status;
     private int type;
 
     public Account() {
     }
 
-    public Account(String userName, String password, String fullName, int isLogined, String status, int type) {
+    public Account(String userName, String password, String fullName, boolean isLogined, String status, int type) {
         this.userName = userName;
         this.password = password;
         this.fullName = fullName;
@@ -35,11 +35,11 @@ public class Account extends AbstractJdbcModel<String> {
         this.fullName = fullName;
     }
 
-    public int getIsLogined() {
+    public boolean getIsLogined() {
         return isLogined;
     }
 
-    public void setIsLogined(int isLogined) {
+    public void setIsLogined(boolean isLogined) {
         this.isLogined = isLogined;
     }
 
@@ -106,7 +106,7 @@ public class Account extends AbstractJdbcModel<String> {
     @Override
     public Object[] getColumnValues() {
         return new Object[]{
-                    userName,
+                    //userName,
                     password,
                     fullName,
                     isLogined,
@@ -121,7 +121,7 @@ public class Account extends AbstractJdbcModel<String> {
             //userName = values[0].toString();
             password = values[0].toString();
             fullName = values[1].toString();
-            isLogined = Integer.parseInt(values[2].toString());
+            isLogined = Boolean.parseBoolean(values[2].toString());
             status = values[3].toString();
             type = Integer.parseInt(values[4].toString());
             
