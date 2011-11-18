6bbf15baec3ccb810436bde4dc81838b057b49e2
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-18 18:01:51 +0000

    Demo using of AdvancedDAO

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/PreSubject.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/PreSubject.java
@@ -1,37 +1,77 @@
 package uit.cnpm02.dkhp.model;
 
+import uit.cnpm02.dkhp.access.advancedJDBC.AdvancedAbstractJdbcModel;
+import uit.cnpm02.dkhp.access.mapper.MapperConstant;
+
 /**
  *
  * @author LocNguyen
  */
-public class PreSubject {
+public class PreSubject extends AdvancedAbstractJdbcModel<PreSubID> {
     /**subject code**/
-    private String subjectCode;
+    private String subId;
     /**prerequisite subject code**/
-    private String preSubjectCode;
+    private String preSubId;
 
     public PreSubject() {
     }
 
-    public PreSubject(String subjectCode, String preSubjectCode) {
-        this.subjectCode = subjectCode;
-        this.preSubjectCode = preSubjectCode;
+    public PreSubject(String subId, String preSubId) {
+        this.subId = subId;
+        this.preSubId = preSubId;
     }
 
-    public String getSubjectCode() {
-        return subjectCode;
+    public String getPreSubId() {
+        return preSubId;
     }
 
-    public void setSubjectCode(String subjectCode) {
-        this.subjectCode = subjectCode;
+    public void setPreSubId(String preSubId) {
+        this.preSubId = preSubId;
     }
 
-    public String getPreSubjectCode() {
-        return preSubjectCode;
+    public String getSubId() {
+        return subId;
     }
 
-    public void setPreSubjectCode(String preSubjectCode) {
-        this.preSubjectCode = preSubjectCode;
+    public void setSubId(String subId) {
+        this.subId = subId;
+    }
+
+    @Override
+    public String[] getColumnNames() {
+        return new String[] {};
+    }
+
+    @Override
+    public Object[] getColumnValues() {
+        return new Object[] {};
+    }
+
+    @Override
+    public void setColumnValues(Object[] values) {
+        //
+    }
+    
+    @Override
+    public String getTableName() {
+        return MapperConstant.DB_NAME
+                + ".monhoctienquyet";
+    }
+
+    @Override
+    public String[] getIdColumnName() {
+        return new String[] {
+            "MaMH",
+            "MaMHTQ"
+        };
+    }
+
+    @Override
+    public Object[] getIdColumnValues() {
+        return new Object[] {
+            subId,
+            preSubId
+        };
     }
     
 }
