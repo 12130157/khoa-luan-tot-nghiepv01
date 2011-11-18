a5bb319cd309e5ca7b1e58d83b6c669d8d28109c
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-18 18:03:56 +0000

    Demo using of AdvancedDAO

---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/DAO/PreSubjectDAO.java
@@ -0,0 +1,13 @@
+package uit.cnpm02.dkhp.DAO;
+
+import uit.cnpm02.dkhp.access.advancedJDBC.AdvancedAbstractJdbcDAO;
+import uit.cnpm02.dkhp.model.PreSubID;
+import uit.cnpm02.dkhp.model.PreSubject;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class PreSubjectDAO extends AdvancedAbstractJdbcDAO<PreSubject, PreSubID> {
+    
+}
