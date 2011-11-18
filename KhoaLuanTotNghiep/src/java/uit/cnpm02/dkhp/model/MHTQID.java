52b15d5df8e8898c52e3feaf709be9dcc4794e76
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-18 15:46:11 +0000

    refactor util package.

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/MHTQID.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.cnpm02.dkhp.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author LocNguyen
+ */
+
+public class MHTQID implements Serializable{
+    public MHTQID() {
+        super();
+    }
+    
+    private String maMH;
+    private String maMHTQ;
+}
+
--- KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/News.java
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/News.java
@@ -2,7 +2,7 @@ package uit.cnpm02.dkhp.model;
 
 import java.util.Date;
 import uit.cnpm02.dkhp.access.mapper.MapperConstant;
-import uit.cnpm02.dkhp.access.util.AbstractJdbcModel;
+import uit.cnpm02.dkhp.access.JDBC.AbstractJdbcModel;
 
 /**
  *
