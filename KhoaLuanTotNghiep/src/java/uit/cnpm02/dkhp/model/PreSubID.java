7a157857ed48893cc5f0fc60c6502b47634ca631
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-18 18:01:51 +0000

    Demo using of AdvancedDAO

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/PreSubID.java
@@ -0,0 +1,55 @@
+package uit.cnpm02.dkhp.model;
+
+import uit.cnpm02.dkhp.access.advancedJDBC.IID;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class PreSubID implements IID {
+
+    private String sudId;
+    private String preSudId;
+
+    public PreSubID() {
+        super();
+    }
+
+    public PreSubID(String sudId, String preSudId) {
+        this.sudId = sudId;
+        this.preSudId = preSudId;
+    }
+
+    public String getPreSudId() {
+        return preSudId;
+    }
+
+    public void setPreSudId(String preSudId) {
+        this.preSudId = preSudId;
+    }
+
+    public String getSudId() {
+        return sudId;
+    }
+
+    public void setSudId(String sudId) {
+        this.sudId = sudId;
+    }
+
+    @Override
+    public String[] getIDNames() {
+        return new String[] {
+          "MaMH",
+          "MaMHTQ"
+        };
+    }
+
+    @Override
+    public Object[] getIDValues() {
+        return new Object[] {
+            sudId,
+            preSudId
+        };
+    }
+
+}
