e6436026908b769da7ac70fbc5a2226667c4728d
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-19 17:56:16 +0000

    Update AdvancedDAO

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/PreSubject.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/PreSubject.java
@@ -8,50 +8,25 @@ import uit.cnpm02.dkhp.access.mapper.MapperConstant;
  * @author LocNguyen
  */
 public class PreSubject extends AdvancedAbstractJdbcModel<PreSubID> {
-    /**subject code**/
-    private String subId;
-    /**prerequisite subject code**/
-    private String preSubId;
 
     public PreSubject() {
     }
 
-    public PreSubject(String subId, String preSubId) {
-        this.subId = subId;
-        this.preSubId = preSubId;
-    }
-
-    public String getPreSubId() {
-        return preSubId;
-    }
-
-    public void setPreSubId(String preSubId) {
-        this.preSubId = preSubId;
-    }
-
-    public String getSubId() {
-        return subId;
-    }
-
-    public void setSubId(String subId) {
-        this.subId = subId;
-    }
-
     @Override
     public String[] getColumnNames() {
-        return new String[] {};
+        return new String[]{};
     }
 
     @Override
     public Object[] getColumnValues() {
-        return new Object[] {};
+        return new Object[]{};
     }
 
     @Override
     public void setColumnValues(Object[] values) {
         //
     }
-    
+
     @Override
     public String getTableName() {
         return MapperConstant.DB_NAME
@@ -60,18 +35,15 @@ public class PreSubject extends AdvancedAbstractJdbcModel<PreSubID> {
 
     @Override
     public String[] getIdColumnName() {
-        return new String[] {
-            "MaMH",
-            "MaMHTQ"
-        };
+        return new String[]{
+                    "MaMH",
+                    "MaMHTQ"
+                };
     }
 
     @Override
     public Object[] getIdColumnValues() {
-        return new Object[] {
-            subId,
-            preSubId
-        };
+        return getId().getIDValues();
     }
-    
+
 }
