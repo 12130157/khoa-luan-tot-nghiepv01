1785a47a9ec90d192e052ded18702a711a023b61
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-19 17:56:49 +0000

    Added constants.java

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/utilities/Constants.java
@@ -0,0 +1,16 @@
+package uit.cnpm02.dkhp.utilities;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class Constants {
+    
+    /**Login role**/
+    public static int ROLE_STUDENT = 0;
+    public static int ROLE_LECTURE = 1;
+    public static int ROLE_PDT = 2;
+    
+    /****/
+    
+}
