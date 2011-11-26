e6de2dd4ac57ced9819020bfa35a556c7e796ee2
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-26 10:14:42 +0000

    Added StudentDao

---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/DAO/StudentDAO.java
@@ -0,0 +1,12 @@
+package uit.cnpm02.dkhp.DAO;
+
+import uit.cnpm02.dkhp.access.JDBC.AbstractJdbcDAO;
+import uit.cnpm02.dkhp.model.Student;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class StudentDAO extends AbstractJdbcDAO<Student, String> {
+    
+}
