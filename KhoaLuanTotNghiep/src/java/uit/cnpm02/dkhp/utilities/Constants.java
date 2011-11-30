40c9148a83e330ad683d742126b6ad7bd9ecc87f
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-11-30 16:38:39 +0000

    Update manager student.

--- KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/utilities/Constants.java
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/utilities/Constants.java
@@ -8,6 +8,19 @@ public class Constants {
     
     /**Account type**/
     public static int ACCOUNT_TYPE_LECTURE = 2;
-    public static int ACCOUNT_TYPE_PDT = 1;
-    public static int ACCOUNT_TYPE_STUDENT = 3;
+    public static int ACCOUNT_TYPE_PDT = 2;
+    public static int ACCOUNT_TYPE_STUDENT = 1;
+    public static String ACCOUNT_STATUS_NORMAL = "Bình thường";
+    public static String ACCOUNT_STATUS_SPECIAL = "Đặc biệt";
+    
+    
+    /**News's type**/
+    public static int NEWS_TYPE_READ = 0;
+    public static int NEWS_TYPE_UNREAD = 1;
+    
+    /**Student's type**/
+    public static int STUDENT_STATUS_NORMAL = 0;
+    public static int STUDENT_STATUS_reservations = 1;
+    
+    //...
 }
