bca1c712e7df0a6f445e0b4139992e463264277a
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-12-03 02:57:30 +0000

    Added Diary

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/DAO/DiaryDAO.java
@@ -0,0 +1,12 @@
+package uit.cnpm02.dkhp.DAO;
+
+import uit.cnpm02.dkhp.access.JDBC.AbstractJdbcDAO;
+import uit.cnpm02.dkhp.model.Diary;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class DiaryDAO  extends AbstractJdbcDAO<Diary, Integer> {
+    
+}
