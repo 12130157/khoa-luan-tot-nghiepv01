2e406b3a2050223a304bea474de0ec618d8eb5cd
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2011-12-07 16:30:08 +0000

    Update student management.

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/ManageStudentController.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/ManageStudentController.java
@@ -49,7 +49,6 @@ public class ManageStudentController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
 
         PrintWriter out = response.getWriter();
