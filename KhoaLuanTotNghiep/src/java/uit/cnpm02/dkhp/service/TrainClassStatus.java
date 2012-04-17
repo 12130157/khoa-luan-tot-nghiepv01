2c4d2db8e06fac0203fb2f41ecb506ddf2e7cf30
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-04-17 15:11:37 +0000

    Add TrainClassStatus

---
+++ KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/service/TrainClassStatus.java
@@ -0,0 +1,33 @@
+package uit.cnpm02.dkhp.service;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public enum TrainClassStatus {
+    OPEN(1, "Opened"), //
+    CANCEL(2, "Canceled"),
+    CLOSE(3, "Closed");
+    
+    private int value;
+    private String description;
+    
+    /**
+     * Initial constructor
+     * @param value value
+     * @param description description string
+     */
+    TrainClassStatus(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+}
