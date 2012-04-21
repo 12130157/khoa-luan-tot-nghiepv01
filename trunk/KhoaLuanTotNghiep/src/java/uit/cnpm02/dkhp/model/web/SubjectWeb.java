579f6db2f050324997a79d56423d4ae9e22947af
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-04-21 04:39:17 +0000

    Add Web Model

---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/model/web/SubjectWeb.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.cnpm02.dkhp.model.web;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class SubjectWeb extends AbstractWeb {
+    
+}
