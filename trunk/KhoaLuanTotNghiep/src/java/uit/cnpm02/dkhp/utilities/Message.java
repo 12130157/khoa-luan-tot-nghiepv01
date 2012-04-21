2999866c1631c6d012d90fcca05ce7061936509c
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-04-21 16:26:14 +0000

    Updad message definition.

---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/utilities/Message.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.cnpm02.dkhp.utilities;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class Message {
+    public static String ADD_TRAINCLASS_SUCCSESS = "Thêm lớp thành công.";
+    public static String ADD_TRAINCLASS_ERROR_CLASS_EXISTED = "Lop da ton tai.";
+    public static String ADD_TRAINCLASS_ERROR_LECTURER_DUPLECATE = "Giang vien khong co san.";
+    public static String ADD_TRAINCLASS_ERROR_ROOM_DUPLECATE = "Phong hoc khong co san.";
+    public static String ADD_TRAINCLASS_ERROR = "Da co loi xay ra trong qua trinh mo lop";
+    
+}
