dad2e99ab4493b3639e68d9791e3acc02126e40b
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-04-25 15:51:33 +0000

    Update Report functional.

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/ReportController.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/ReportController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import uit.cnpm02.dkhp.model.Student;
+import uit.cnpm02.dkhp.model.TrainClass;
 import uit.cnpm02.dkhp.service.IReporter;
 import uit.cnpm02.dkhp.service.impl.ReporterImpl;
 import uit.cnpm02.dkhp.utilities.Message;
@@ -25,13 +26,15 @@ public class ReportController extends HttpServlet {
     private IReporter reportService = new ReporterImpl();
 
     /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and
+     * <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request,
+                                        HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -42,11 +45,13 @@ public class ReportController extends HttpServlet {
              requestAction = ReportFunctionSupported.DEFAULT.getValue();
         }
         try {
-            if (requestAction.equals(ReportFunctionSupported.DEFAULT.getValue())) {
+            if (requestAction.equals(ReportFunctionSupported.
+                                                DEFAULT.getValue())) {
                 String path = "./jsps/PDT/Report.jsp";
                 response.sendRedirect(path);
                 return;
-            } else if (requestAction.equals(ReportFunctionSupported.SEARCH_STUDENT.getValue())) {
+            } else if (requestAction.equals(ReportFunctionSupported.
+                                                SEARCH_STUDENT.getValue())) {
                 // List existed TrainClass, Pagging setup
                 String key = request.getParameter("value");
                 if (key != null) {
@@ -60,9 +65,19 @@ public class ReportController extends HttpServlet {
                     out.println(Message.STUDENT_SEARCH_NOTFOUND);
                 }
                 return;
+            } else if (requestAction.equals(ReportFunctionSupported.
+                                                STUDENT_REPORT.getValue())) {
+                String mssv = request.getParameter("value");
+                List<TrainClass> trainClassReg = getStudentReport(mssv);
+                if ((trainClassReg != null) && !trainClassReg.isEmpty()) {
+                    writeStudentReportDetail(trainClassReg, out);
+                } else {
+                    out.println(Message.STUDENT_REPORT_NO_REPORT);
+                }
             }
         } catch(Exception ex) {
-            Logger.getLogger(ReportController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ReportController.class.getName()).
+                                                log(Level.SEVERE, null, ex);
         } finally {
             out.close();
         }
@@ -109,6 +124,11 @@ public class ReportController extends HttpServlet {
         return results;
     }
     
+    /**
+     * Data respone throught ajax when call search student from page
+     * @param datas list student found
+     * @param out output stream
+     */
     private void writeRespond(List<Student> datas, PrintWriter out) {
         out.println("<table id = \"list-student\" name = \"list-student\">");
 
@@ -119,12 +139,41 @@ public class ReportController extends HttpServlet {
         for (int i = 0; i < datas.size(); i++) {
             out.println("<tr>");
             out.println("<td> " + (i + 1) + " </td>");
-            out.println("<td> " + datas.get(i).getFullName() + " </td>");
-            out.println("</tr>");
+            out.println("<td> <a href=\"#\" onclick=getDetailStudentReport("
+                    + datas.get(i).getId()
+                    + ")>" + datas.get(i).getFullName()
+                    + "</a> </td>");
+            out.println("</tr>");// <a hreft > abc </a>
         }
         out.println("</table>");
     }
+
+    private List<TrainClass> getStudentReport(String mssv) {
+        return reportService.getTrainClassRegistered(mssv);
+    }
     
+    private void writeStudentReportDetail(List<TrainClass> datas,
+                                                        PrintWriter out) {
+        out.println("<table id = \"student-report\" name = \"student-report\">");
+
+        out.println("<tr>"
+                + "<th> STT </th>"
+                + "<th> Ma lop </th>"
+                + "<th> Mon hoc </th>"
+                + "<th> Nam hoc </th>"
+                + "<th> Hoc ky </th>"
+                + "</tr>");
+        for (int i = 0; i < datas.size(); i++) {
+            out.println("<tr>");
+            out.println("<td> " + (i + 1) + " </td>");
+            out.println("<td>" + datas.get(i).getId().getClassCode() + "</td>");
+            out.println("<td> " + datas.get(i).getSubjectName() + " </td>");
+            out.println("<td> " + datas.get(i).getId().getYear() + " </td>");
+            out.println("<td> " + datas.get(i).getId().getSemester() + " </td>");
+            out.println("</tr>");// <a hreft > abc </a>
+        }
+        out.println("</table>");
+    }
 
      /**
      * An enum define all supported function of serverlet
@@ -132,7 +181,8 @@ public class ReportController extends HttpServlet {
      */
     public enum ReportFunctionSupported {
         DEFAULT("default"), // List first page of class opened.
-        SEARCH_STUDENT("search_student");
+        SEARCH_STUDENT("search_student"),
+        STUDENT_REPORT("getDetailStudentReport");
         //CREATE("create"),   // Create new class form support
         //DELETE("delete"),   // Remove class
         //UPDATE("update");   // Update
