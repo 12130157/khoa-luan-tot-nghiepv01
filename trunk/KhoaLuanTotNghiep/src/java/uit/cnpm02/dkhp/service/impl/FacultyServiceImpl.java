4b5ce60a87fc4c157fdcc082d9d4deb959c009ee
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-05-04 09:16:12 +0000

    Update Subject Service.

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/service/impl/FacultyServiceImpl.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/service/impl/FacultyServiceImpl.java
@@ -1,11 +1,14 @@
 package uit.cnpm02.dkhp.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import uit.cnpm02.dkhp.DAO.DAOFactory;
 import uit.cnpm02.dkhp.DAO.FacultyDAO;
+import uit.cnpm02.dkhp.DAO.SubjectDAO;
 import uit.cnpm02.dkhp.model.Faculty;
+import uit.cnpm02.dkhp.model.Subject;
 import uit.cnpm02.dkhp.service.IFacultyService;
 
 /**
@@ -14,9 +17,11 @@ import uit.cnpm02.dkhp.service.IFacultyService;
  */
 public class FacultyServiceImpl implements IFacultyService {
     private FacultyDAO fDao;
+    private SubjectDAO subjectDao;
     
     public FacultyServiceImpl() {
         fDao = DAOFactory.getFacultyDao();
+        subjectDao = DAOFactory.getSubjectDao();
     }
 
     @Override
@@ -28,6 +33,19 @@ public class FacultyServiceImpl implements IFacultyService {
                     .log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+
+    @Override
+    public List<Subject> getSubject(String faculty) {
+        List<Subject> results = new ArrayList<Subject>(10);
+        try {
+            results = subjectDao.findAll();
+            //results = subjectDao.findByColumName("MaKhoa", faculty);
+        } catch (Exception ex) {
+            Logger.getLogger(FacultyServiceImpl.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return results;
     }
     
 }
