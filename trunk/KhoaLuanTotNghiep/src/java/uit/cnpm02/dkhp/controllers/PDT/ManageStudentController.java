09d6101d7ec1262312df4288c1f11ba65c5cdb86
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-05-12 04:29:36 +0000

    Update Manage Student.

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/ManageStudentController.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/ManageStudentController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
@@ -46,7 +45,6 @@ public class ManageStudentController extends HttpServlet {
     private IStudentService studentService = new StudentServiceImpl();
 
     private StudentDAO studentDao = DAOFactory.getStudentDao();
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
     private int rowPerPage = Constants.ELEMENT_PER_PAGE_DEFAULT;
     private int numPage = 1;
     private int currentPage = 1;
@@ -71,6 +69,7 @@ public class ManageStudentController extends HttpServlet {
         try {
             session.removeAttribute("error");
             session.removeAttribute("students_added");
+            session.removeAttribute("import.from.file.response");
         } catch (Exception ex) {
             //
         }
@@ -78,7 +77,7 @@ public class ManageStudentController extends HttpServlet {
             numPage = getNumberPage();
             session.setAttribute("numpage", numPage);
             String action = request.getParameter("function");
-            String datas = request.getParameter("data");
+            //String datas = request.getParameter("data");
             if (action.equalsIgnoreCase("liststudent")) {
                 listStudent(request, response);
                 String path = "./jsps/PDT/ListStudent.jsp";
@@ -102,28 +101,14 @@ public class ManageStudentController extends HttpServlet {
             } else if (action.equalsIgnoreCase(ManageStudentSupport
                                                 .CANCEL_ADD_ONE.getValue())) {
                 doCancelOne(out, request);
-            }
-            /* else if (action.equalsIgnoreCase("import")) {
-                //Data input in format: student1; student2; student 3 ...
-                //Student: mssv, hoten, ...
-                ExecuteResult result = importStudentFromDataString(datas);
-                if (!result.isIsSucces()) {
-                    out.println("Đã có lỗi xảy ra: " + result.getMessage());
-                } else {
-                    List<Student> students = (List<Student>)result.getData();
-                    out.println("<u>Thêm thành công sinh viên:</u></br>");
-                    wirteOutListStudent(out, students);
-                }
-            }*/ else if (action.equalsIgnoreCase("importfromfile")) {
-                ExecuteResult result = importStudentFromFile(request, response);
+            } else if (action.equalsIgnoreCase(ManageStudentSupport
+                                                .IMPORT_FROM_FILE.getValue())) {
+                doImportFromFile(request, response);
                 
-                if (!result.isIsSucces()) {
-                    session.setAttribute("error", result.getMessage());
-                } else {
-                    session.setAttribute("students_added", (List<Student>)result.getData());
-                }
-                String path = "./jsps/PDT/ImportStudent.jsp";
-                response.sendRedirect(path);
+            } else if (action.equalsIgnoreCase(ManageStudentSupport
+                                                .RETRY_IMPORT_FROM_FILE.getValue())) {
+                doRetryImportFromFile(request, response);
+                
             } else if (action.equalsIgnoreCase("delete")) {
                 String result = deleteStudent(request, response);
                 session.setAttribute("error", result);
@@ -194,61 +179,6 @@ public class ManageStudentController extends HttpServlet {
     }
 
     /**
-     * Import data form a String
-     * @param datas data
-     * @return
-     * @throws Exception 
-     */
-    private ExecuteResult importStudentFromDataString(String datas) throws Exception {
-        if ((datas == null) || (datas.length() < 1)) {
-            return new ExecuteResult(false, "Vui lòng kiểm tra lại dữ liệu nhập");
-        }
-
-        List<Student> students = new ArrayList<Student>(10);
-        String[] studentsStr = datas.split(";");
-        if (studentsStr != null) {
-            for (int i = 0; i < studentsStr.length; i++) {
-                String[] studentDetail = studentsStr[i].split(",");
-                Student s = new Student();
-                s.setId(studentDetail[0]);                        //MSSV        0
-                s.setFullName(studentDetail[1]);                  //Ho Ten      1
-                Date startDate = dateFormat.parse(studentDetail[2]);
-                s.setDateStart(startDate);                         //Ngay Nhap Hoc 14
-                s.setBirthday(startDate);                         //Ngay Sinh   2 
-                s.setGender(studentDetail[3]);                    //Gioi Tinh      3
-                s.setIdentityNumber(studentDetail[4]);            //CMND   4
-                s.setHomeAddr(studentDetail[5]);                  //Que quan       5
-                s.setAddress(studentDetail[6]);                   //Dia chi        6
-                s.setPhone(studentDetail[7]);                     //Dien thoai     7
-                s.setEmail(studentDetail[8]);                     //Email          8
-                s.setClassCode(studentDetail[9]);                 //Lop           9
-                s.setFacultyCode(studentDetail[10]);              //Khoa          10
-                s.setCourseCode(studentDetail[11]);               //KhoaHoc       11
-                s.setStatus(studentDetail[12]);                   //TinhTrang     12
-                s.setStudyLevel(studentDetail[13]);               //Bac hoc       13
-                Date birthDay = dateFormat.parse(studentDetail[14]);
-                s.setBirthday(birthDay);                         //Ngay Sinh   2 
-                
-                s.setStudyType(studentDetail[15]);                //Loai hinh hoc 15
-                s.setNote(studentDetail[16]);                     //Ghi chu       16
-
-                students.add(s);
-            }
-        }
-
-        Collection<String> id_Students = DAOFactory.getStudentDao().addAll(students);
-
-        if (id_Students == null) {
-            return new ExecuteResult(false, "Thêm không thành công");
-        }
-        
-        ExecuteResult er = new ExecuteResult(true, "");
-        er.setData(students);
-
-        return er;
-    }
-
-    /**
      * Import data from file
      * Data format must follow a specified rule.
      * @param request request object
@@ -256,67 +186,43 @@ public class ManageStudentController extends HttpServlet {
      * @return string
      * @throws Exception 
      */
-    private ExecuteResult importStudentFromFile(HttpServletRequest request,
-                            HttpServletResponse response) throws Exception {
+    private ExecuteResult importStudentFromFile(HttpServletRequest request
+                , boolean importAsPossible) throws Exception {
         List<Student> students = new ArrayList<Student>();
-        try {
-            HSSFWorkbook wb = FileUtils.getWorkbook(request, response);
-            HSSFSheet sheet = wb.getSheetAt(0);
-            HSSFRow rowTemp;
-            HSSFCell cellTemp;
 
-            int cellType;
-            Iterator rowIter = sheet.rowIterator();
-            while (rowIter.hasNext()) {
-                rowTemp = (HSSFRow) rowIter.next();
-                if (rowTemp == null) {
-                    continue;
-                }
-                cellTemp = rowTemp.getCell(0);
-                if (cellTemp == null) {
-                    continue;
-                }
+        HSSFWorkbook wb = FileUtils.getWorkbook(request);
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow rowTemp;
+        HSSFCell cellTemp;
 
-                cellTemp = rowTemp.getCell(0);
-                cellType = cellTemp.getCellType();
-                //check the first cell of data must be a number
-                if (cellType != HSSFCell.CELL_TYPE_NUMERIC) {
-                    continue;
-                } else {
-                    Student s = initStudentFromHSSFRow(rowTemp);
-                    students.add(s);
-                }
+        int cellType;
+        Iterator rowIter = sheet.rowIterator();
+        while (rowIter.hasNext()) {
+            rowTemp = (HSSFRow) rowIter.next();
+            if (rowTemp == null) {
+                continue;
+            }
+            cellTemp = rowTemp.getCell(0);
+            if (cellTemp == null) {
+                continue;
             }
 
-            List<Student> existedStudents = new ArrayList<Student>(10);
-            StudentDAO sDao = DAOFactory.getStudentDao();
-            for (Student s : students) {
-                if (sDao.findById(s.getId()) != null) {
-                    existedStudents.add(s);
-                }
+            cellTemp = rowTemp.getCell(0);
+            cellType = cellTemp.getCellType();
+            //check the first cell of data must be a number
+            if (cellType != HSSFCell.CELL_TYPE_NUMERIC) {
+                continue;
+            } else {
+                Student s = initStudentFromHSSFRow(rowTemp);
+                students.add(s);
             }
-            
-            if (existedStudents.size() > 0) {
-                String shortData = "Mã SV đã tồn tại: ";
-                for (Student ss : existedStudents) {
-                    shortData += ss.getId() + "; ";
-                }
-                
-                return new ExecuteResult(false, shortData);
-            }
-            
-            Collection<String> id_Students = sDao
-                    .addAll(students);
-            //if (id_Students == null) {
-            //    return new ExecuteResult(false, "Thêm không thành công");
-            //}
-            ExecuteResult result = new ExecuteResult(true, "");
-            result.setData(students);
-            return result;
-
-        } catch (Exception ex) {
-            return new ExecuteResult(false, "Thêm không thành công. Lỗi: " + ex.toString());
         }
+
+        if (!students.isEmpty()) {
+            return studentService.addStudents(students, importAsPossible,
+                                                request.getSession().getId());
+        }
+        return new ExecuteResult(false, "Không thể đọc thông tin.");
     }
 
     private Student initStudentFromHSSFRow(HSSFRow rowTemp) {
@@ -719,7 +625,7 @@ public class ManageStudentController extends HttpServlet {
          writeOutCancelAddOneResult(out, er);
     }
     
-     private void writeOutCancelAddOneResult(PrintWriter out, ExecuteResult er) {
+    private void writeOutCancelAddOneResult(PrintWriter out, ExecuteResult er) {
         if (!er.isIsSucces()) {
             out.println("Không thể xóa SV. " + er.getMessage());
             out.println("Vui lòng truy cập trang </b>Quản lý Sinh viên</b> để thử lại");
@@ -728,12 +634,70 @@ public class ManageStudentController extends HttpServlet {
             out.println("Xóa thành công sinh viên <b>" + s.getFullName() + "</b>");
         }
     }
+
+    private void doImportFromFile(HttpServletRequest request,
+                            HttpServletResponse response) throws Exception {
+        boolean importAsPossible;
+        try {
+            importAsPossible = Boolean.parseBoolean(
+                                request.getParameter("import-as-possible"));
+        } catch (Exception ex) {
+            importAsPossible = false;
+        }
+        ExecuteResult result = importStudentFromFile(request, importAsPossible);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("import.from.file.response", true);
+        session.setAttribute("import-from-file-result", result);
+        String path = "./jsps/PDT/ImportStudent.jsp";
+        response.sendRedirect(path);
+    }
+
+    private void doRetryImportFromFile(HttpServletRequest request,
+            HttpServletResponse response) {
+        try {
+            ExecuteResult er = studentService.addStudents(
+                            null, true, request.getSession().getId());
+            writeOutResponseForRetryImportFromFile(response.getWriter(), er);
+        } catch (IOException ex) {
+            Logger.getLogger(ManageStudentController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void writeOutResponseForRetryImportFromFile(PrintWriter out,
+            ExecuteResult er) {
+        if (!er.isIsSucces()) {
+            out.println(er.getMessage());
+        } else {
+            List<Student> students = (List<Student>) er.getData();
+            if ((students == null) || students.isEmpty()) {
+                out.println("Không có SV nào được thêm");
+            } else {
+                out.println("<i>Thêm thành công các SV:</i>");
+                out.println("<table class=\"general-table\" style=\"width: 450px;\">");
+                out.println("<tr><th>STT</th><th>MSSV</th><th>Họ và tên</th><th>CMND</th></tr>");
+                for (int i = 0; i < students.size(); i++) {
+                    out.println("<tr>"
+                            + "<td>" + (i+1) + "</td>"
+                            + "<td>" + students.get(i).getId() + "</td>"
+                            + "<td>" + students.get(i).getFullName() + "</td>"
+                            + "<td>" + students.get(i).getIdentityNumber() + "</td>"
+                            + "</tr>");
+                }
+                out.println("</table>");
+            }
+        }
+    }
+    
     
     public enum ManageStudentSupport {
         DEFAULT("default"), // List first page of class opened.
         PRE_IMPORT("pre-import-student"),
         VALIDATE_ADD_ONE("validate-add-one"),
         ADD_ONE("add-one"),
+        IMPORT_FROM_FILE("importfromfile"),
+        RETRY_IMPORT_FROM_FILE("retry-import-from-file"),
         CANCEL_ADD_ONE("cancel-add-one"),
         FACULTY_CHANGED("faculty-change");
         
@@ -748,3 +712,7 @@ public class ManageStudentController extends HttpServlet {
         
     }
 }
+
+
+
+// importAsPossible = Boolean.parseBoolean(request.getParameter("import-as-possible"));
