565a595c6a8f007af37f64cf39a40d3cedbc5e1b
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-05-26 15:34:36 +0000

    Update Lecturer functions.

---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/DAO/FileInfoDAO.java
@@ -0,0 +1,12 @@
+package uit.cnpm02.dkhp.DAO;
+
+import uit.cnpm02.dkhp.access.JDBC.AbstractJdbcDAO;
+import uit.cnpm02.dkhp.model.FileInfo;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class FileInfoDAO extends AbstractJdbcDAO<FileInfo, Integer> {
+    
+}
---
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/DAO/TaskDAO.java
@@ -0,0 +1,12 @@
+package uit.cnpm02.dkhp.DAO;
+
+import uit.cnpm02.dkhp.access.JDBC.AbstractJdbcDAO;
+import uit.cnpm02.dkhp.model.Task;
+
+/**
+ *
+ * @author LocNguyen
+ */
+public class TaskDAO extends AbstractJdbcDAO<Task, Integer> {
+    
+}
