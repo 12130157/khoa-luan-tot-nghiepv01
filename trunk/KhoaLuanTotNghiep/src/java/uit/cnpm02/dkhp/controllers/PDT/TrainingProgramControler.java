2b4eb724e06b16e5c4cd8bd46bddae72b3f9a924
locnv.uit@gmail.com <locnv.uit@gmail.com@4fed32ce-0ea2-0307-a519-3737dca7f712>
2012-07-14 15:48:05 +0000

    Update TrainProgram manager

--- trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/TrainingProgramControler.java
+++ trunk/KhoaLuanTotNghiep/src/java/uit/cnpm02/dkhp/controllers/PDT/TrainingProgramControler.java
@@ -6,6 +6,7 @@ package uit.cnpm02.dkhp.controllers.PDT;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -25,7 +26,9 @@ import uit.cnpm02.dkhp.DAO.TrainProgDetailDAO;
 import uit.cnpm02.dkhp.DAO.TrainProgramDAO;
 import uit.cnpm02.dkhp.model.Course;
 import uit.cnpm02.dkhp.model.Faculty;
+import uit.cnpm02.dkhp.model.Subject;
 import uit.cnpm02.dkhp.model.TrainProDetail;
+import uit.cnpm02.dkhp.model.TrainProDetailID;
 import uit.cnpm02.dkhp.model.TrainProgram;
 import uit.cnpm02.dkhp.utilities.StringUtils;
 
@@ -58,8 +61,11 @@ public class TrainingProgramControler extends HttpServlet {
                 createNewTrainProg(request, response);
             } else if (action.equalsIgnoreCase("pre-update-train-prog")) {
                 preUpdateTrainProg(request, response);
-            }
-            
+            } else if (action.equalsIgnoreCase("add-subject-to-traing-prog")) {
+                addSubjectToTrainProg(request, response);
+            } else if (action.equalsIgnoreCase("remove-subject-to-traing-prog")) {
+                deleteSubFromTrainProg(request, response);
+            }           
         } finally {            
             out.close();
         }
@@ -124,7 +130,8 @@ public class TrainingProgramControler extends HttpServlet {
         response.sendRedirect(path);
     }
 
-    private void getTrainProgDetail(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    private void getTrainProgDetail(HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
         String trainProgID = request.getParameter("train-program-ID");
         PrintWriter out = response.getWriter();
         if (StringUtils.isEmpty(trainProgID)) {
@@ -135,18 +142,22 @@ public class TrainingProgramControler extends HttpServlet {
         TrainProgDetailDAO tpdDao = DAOFactory.getTrainProgDetailDAO();
         try {
             List<TrainProDetail> tps = tpdDao.findByColumName("MaCTDT", trainProgID);
-            Collections.sort(tps, new Comparator<TrainProDetail>() {
-
-                @Override
-                public int compare(TrainProDetail o1, TrainProDetail o2) {
-                    return o1.getSemester() - o2.getSemester();
-                }
-            });
+            sortListTrainProDetailBySemeter(tps);
             writeOutTrainProDetail(out, tps, trainProgID);
         } catch (Exception ex) {
             Logger.getLogger(TrainingProgramControler.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+    }
+    
+    private void sortListTrainProDetailBySemeter(List<TrainProDetail> tpds) {
+            Collections.sort(tpds, new Comparator<TrainProDetail>() {
+
+            @Override
+            public int compare(TrainProDetail o1, TrainProDetail o2) {
+                return o1.getSemester() - o2.getSemester();
+            }
+        });
     }
 
     private void writeOutTrainProDetail(PrintWriter out,
@@ -174,6 +185,7 @@ public class TrainingProgramControler extends HttpServlet {
         }
         //<form action="" name="formstudent"  id="formstudent">
         out.println("<form id=\"form-update\" name=\"form-update\" action=\"../../TrainingProgramControler?action=pre-update-train-prog\" method=\"post\">"
+                    + "<input type=\"hidden\" name=\"train-prog-ID\" id=\"train-prog-ID\" value=\"" + trainProgId + "\" />"
                         /*+ "<div class=\"button-1\">"
                         + "<input type= \"submit\" ><img src=\"../../imgs/check.png\"/>Cập nhật</input>"
                         + "</div>"*/
@@ -229,10 +241,193 @@ public class TrainingProgramControler extends HttpServlet {
         }
     }
 
+    TrainProgDetailDAO tpdDao = DAOFactory.getTrainProgDetailDAO();
     private void preUpdateTrainProg(HttpServletRequest request,
             HttpServletResponse response) throws IOException {
+        //PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        // Load ds lop trong ctdt
+        String trainProgID = request.getParameter("train-prog-ID");
+        session.setAttribute("train-prog-ID", trainProgID);
+        List<String> subIdExisted = new ArrayList<String>(10);
+        try {
+            List<TrainProDetail> tps = tpdDao.findByColumName("MaCTDT", trainProgID);
+            sortListTrainProDetailBySemeter(tps);
+            SubjectDAO subDao = DAOFactory.getSubjectDao();
+            if (tps != null && !tps.isEmpty()) {
+                for (TrainProDetail tcd : tps) {
+                    String subName = tcd.getId().getSubjectID();
+                    subIdExisted.add(tcd.getId().getSubjectID());
+                    try {
+                        subName = subDao.findById(tcd.getId().getSubjectID()).getSubjectName();
+                        tcd.setSubjectName(subName);
+                    } catch (Exception ex) {
+                        //
+                    }
+                }
+            }
+            session.setAttribute("lst-train-pro-detail", tps);
+            
+            List<Subject> allSubs = subDao.findAll();
+            List<Subject> tobeRemove = new ArrayList<Subject>(10);
+            for (Subject s : allSubs) {
+                if (subIdExisted.contains(s.getId())) {
+                    tobeRemove.add(s);
+                }
+            }
+            if (tobeRemove != null && !tobeRemove.isEmpty()) {
+                allSubs.removeAll(tobeRemove);
+            }
+            session.setAttribute("all-subjects", allSubs);
+        } catch (Exception ex) {
+            Logger.getLogger(TrainingProgramControler.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        // Load ds lop chua co trong ctdt
         String path = "./jsps/PDT/UpdateTrainProgram.jsp";
         response.sendRedirect(path);
     }
+    
+    private List<TrainProDetail> getTrainProgDetail(String trainProgID) throws Exception {
+        List<TrainProDetail> tps = tpdDao.findByColumName("MaCTDT", trainProgID);
+            sortListTrainProDetailBySemeter(tps);
+            SubjectDAO subDao = DAOFactory.getSubjectDao();
+            if (tps != null && !tps.isEmpty()) {
+                for (TrainProDetail tcd : tps) {
+                    String subName = tcd.getId().getSubjectID();
+                    try {
+                        subName = subDao.findById(tcd.getId().getSubjectID()).getSubjectName();
+                        tcd.setSubjectName(subName);
+                    } catch (Exception ex) {
+                        //
+                    }
+                }
+            }
+            
+            return tps;
+    }
 
+    private void addSubjectToTrainProg(HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        // List subject in format: subID-course;subID-course;...
+        String listSubject = request.getParameter("data");
+        String trainProgID = request.getParameter("trainProgID");
+        if (StringUtils.isEmpty(listSubject)) {
+            out.println("Lỗi: Không có môn học nào được chọn.");
+            return;
+        }
+        
+        if (StringUtils.isEmpty(trainProgID)) {
+            out.println("Lỗi: Không tìm thấy CTĐT tương ứng.");
+            return;
+        }
+        String[] subDatas = listSubject.split(";");
+        List<TrainProDetail> trainProgDetails = new ArrayList<TrainProDetail>(10);
+        for (int i = 0; i < subDatas.length; i++) {
+            String[] detail = subDatas[i].split("-"); // Sub-course
+            try {
+                TrainProDetail tpd = new TrainProDetail(
+                        trainProgID, detail[0], Integer.parseInt(detail[1]));
+                trainProgDetails.add(tpd);
+            } catch (Exception ex) {
+                continue;
+            } 
+        }
+        
+        try {
+            List<TrainProDetailID> tpdIDs = new ArrayList<TrainProDetailID>(10);
+            for (TrainProDetail tpdTemp : trainProgDetails) {
+                TrainProDetailID id = new TrainProDetailID(trainProgID, tpdTemp.getId().getSubjectID());
+                tpdIDs.add(id);
+            }
+            List<TrainProDetail> tpdExisted = tpdDao.findByIds(tpdIDs); 
+            if (tpdExisted != null && !tpdExisted.isEmpty()) {
+                trainProgDetails.removeAll(tpdExisted);
+            }
+            
+            // Process duplecate subject per each TrainProgram
+            List<TrainProDetail> tobeAdd = new ArrayList<TrainProDetail>(10);
+            for (TrainProDetail t : trainProgDetails) {
+                if (!tobeAdd.contains(t)) {
+                    tobeAdd.add(t);
+                }
+            }
+            
+            if (tobeAdd != null && !tobeAdd.isEmpty())
+                tpdDao.addAll(tobeAdd);
+            
+            session.setAttribute("lst-train-pro-detail", getTrainProgDetail(trainProgID));
+            
+            out.println("Cập nhật thành công, F5 để xem cập nhật");
+        } catch (Exception ex) {
+            Logger.getLogger(
+                    TrainingProgramControler.class.getName()).log(Level.SEVERE, null, ex);
+            out.println("Lỗi: " + ex.toString());
+        }
+    }
+
+    private void deleteSubFromTrainProg(HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        // List subject in format: subId1;subId2; ...
+        String listSubject = request.getParameter("data");
+        String trainProgID = request.getParameter("trainProgID");
+        if (StringUtils.isEmpty(listSubject)) {
+            out.println("Lỗi: Không có môn học nào được chọn.");
+            return;
+        }
+        
+        if (StringUtils.isEmpty(trainProgID)) {
+            out.println("Lỗi: Không tìm thấy CTĐT tương ứng.");
+            return;
+        }
+        String[] subDatas = listSubject.split(";");
+        List<TrainProDetail> trainProgDetails = new ArrayList<TrainProDetail>(10);
+        List<TrainProDetailID> ids = new ArrayList<TrainProDetailID>(10);
+        for (int i = 0; i < subDatas.length; i++) {
+            TrainProDetailID id = new TrainProDetailID(trainProgID, subDatas[i]);
+            ids.add(id);
+        }
+        
+        try {
+            trainProgDetails = tpdDao.findByIds(ids);
+            if (trainProgDetails != null)
+                tpdDao.delete(trainProgDetails);
+            
+            trainProgDetails = getTrainProgDetail(trainProgID);
+            session.setAttribute("lst-train-pro-detail", trainProgDetails);
+            writeListTrainProgramDetail(out, trainProgDetails);
+        } catch (Exception ex) {
+            Logger.getLogger(TrainingProgramControler.class.getName())
+                    .log(Level.SEVERE, null, ex);
+            out.println("Error: " + ex.toString());
+        }
+    }
+
+    private void writeListTrainProgramDetail(PrintWriter out, List<TrainProDetail> tpds) {
+        out.println("<table id=\"tbl-existed\" class=\"general-table\" style=\"float: left; width: 520px; maring-left: 25px;\">");
+        out.println("<tr><th>STT</th><th>Mã Môn học</th><th>Môn học</th><th>Học kỳ</th><th></th></tr>");
+        if (tpds != null && !tpds.isEmpty()) {
+            int counter = 0;
+            for (TrainProDetail tpd : tpds) {
+                out.println("<tr id=\"row_tpd" + tpd.getId().getSubjectID() + "\">"
+                        + "<td>" + (counter++) + "</td>"
+                        + "<td>" + tpd.getId().getSubjectID() + "</td>"
+                        + "<td>" + tpd.getSubjectName() + "</td>"
+                        + "<td>" + tpd.getSemester() + "</td>"
+                        + "<td>"
+                        + "<input "
+                            + "type=\"checkbox\" id=\"chk" + (counter-1) + "\""
+                            + " onclick=\"highLightSelectRow(this, 'row_tpd" + tpd.getId().getSubjectID() + "')\"/>"
+                        + "</td>"
+                        + "</tr>");
+            }
+        
+        }
+        out.println("</table>");
+    }
 }
